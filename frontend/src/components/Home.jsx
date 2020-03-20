b0a6c05d2999c18c0e49085658608a448536b8cf
unknown <nikitamenon2510@gmail.com>
2020-03-21 05:28:21 +0530

    fix team section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 	return (
 		<div className='nav'>
 			<nav className="nav-wrapper transparent accent-1">
-				<a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons grey-text text-darken-3">menu</i></a>
+				<a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons" style={{'color':'#C33427'}}>menu</i></a>
 				<div className="container">
 					<ul className="right hide-on-small-and-down">
 						<li><Link className='grey-text text-darken-3' activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={400}>Home</Link></li>
@@ -31,13 +31,13 @@ const Navbar = () => {
 				</div>
 			</nav>
 
-			<ul id="slide-out" className="sidenav">
-				<li><Link className='grey-text text-darken-3' activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={400}>Home</Link></li>
-				<li><Link className='grey-text text-darken-3' activeClass="active" to="About" spy={true} smooth={true} offset={-70} duration={400}>About</Link></li>
-				<li><Link className='grey-text text-darken-3' activeClass="active" to="achievements" spy={true} smooth={true} offset={-70} duration={400}>Achievements</Link></li>
-				<li><Link className='grey-text text-darken-3' activeClass="active" to="gallery" spy={true} smooth={true} offset={-70} duration={400}>Gallery</Link></li>
-				<li><Link className='grey-text text-darken-3' activeClass="active" to="team" spy={true} smooth={true} offset={-70} duration={400}>Our Team</Link></li>
-				<li><Link className='grey-text text-darken-3' activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={400}>Contact</Link></li>
+			<ul id="slide-out" className="sidenav" style={{'background-color':'black','padding':'10vh', 'text-align':'center', 'width':'100%'}}>
+				<li><Link className='grey-text' activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={400}>Home</Link></li>
+				<li><Link className='grey-text' activeClass="active" to="About" spy={true} smooth={true} offset={-70} duration={400}>About</Link></li>
+				<li><Link className='grey-text' activeClass="active" to="achievements" spy={true} smooth={true} offset={-70} duration={400}>Achievements</Link></li>
+				<li><Link className='grey-text' activeClass="active" to="gallery" spy={true} smooth={true} offset={-70} duration={400}>Gallery</Link></li>
+				<li><Link className='grey-text' activeClass="active" to="team" spy={true} smooth={true} offset={-70} duration={400}>Our Team</Link></li>
+				<li><Link className='grey-text' activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={400}>Contact</Link></li>
 			</ul>
 		</div>
 	)
