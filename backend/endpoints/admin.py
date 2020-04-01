985b31c8bced6dd69ab744ca7c39d2a8af382c28
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-01 12:39:38 +0000

    addcreate admin base

--- backend/endpoints/admin.py
+++ backend/endpoints/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Announcements
 
 
-class AnnouncementAdmin:
+class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
 admin.site.register(Announcements , AnnouncementAdmin)
