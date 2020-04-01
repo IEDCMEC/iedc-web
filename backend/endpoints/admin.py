5b399b7a55ed3b9391edf80bfd109fe25f68e813
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-01 18:09:15 +0000

    all changes working

--- backend/endpoints/admin.py
+++ backend/endpoints/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Announcements
+from .models import Announcements, ContactForm, ImageService
 
 
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
+class contactFormAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email','message','completed')
+class ImageServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'image')
+
 admin.site.register(Announcements , AnnouncementAdmin)
+admin.site.register(ContactForm , contactFormAdmin)
+admin.site.register(ImageService , ImageServiceAdmin)
