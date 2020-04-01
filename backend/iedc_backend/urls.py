26c73e8b7527d32faf382e2576fabc21ecbe20fc
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-01 18:21:42 +0000

    added url routes

--- backend/iedc_backend/urls.py
+++ backend/iedc_backend/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from rest_framework import routers
 from endpoints import views
 
-router = routers.DefaultRouter()                      # add this
+router = routers.DefaultRouter()                      
 router.register(r'announcements', views.AnnouncementsView, 'Announcements') 
+router.register(r'contactform', views.ContactFormView, 'ContactForm') 
+router.register(r'imageupload', views.ImageServiceView, 'ImageService') 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
