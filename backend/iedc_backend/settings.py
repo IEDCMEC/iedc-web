4edc67bd832152fba8d31746b0956e3b2a19ea0f
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-01 12:50:03 +0000

    added whitelist for corsheaders

--- backend/iedc_backend/settings.py
+++ backend/iedc_backend/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+         'http://localhost:3000/',
+         'https://localhost:3000/'
+]
