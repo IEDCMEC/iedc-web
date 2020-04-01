e57f501c7154568abbc79c458119d4a40c39baf5
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-04-01 16:08:49 +0000

    working api

--- backend/iedc_backend/urls.py
+++ backend/iedc_backend/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from rest_framework import routers
 from endpoints import views
 
 router = routers.DefaultRouter()                      # add this
-router.register(r'Announcements', views.AnnouncementsView, 'Announcements') 
+router.register(r'announcements', views.AnnouncementsView, 'Announcements') 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
