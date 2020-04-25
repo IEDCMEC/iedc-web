161286ed95a3af1485c0c18762f593dbe9a90304
artibhatia13 <bhatiaarti138@gmail.com>
2020-04-26 01:23:30 +0530

    contact and announcements

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,72 +1,96 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 import "../index.css";
 
-const Contact = () => {
-  return (
-    <div id="top_margin">
-      <section id="box_curve_top">
+class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      phone: '',
+      email: '',
+      message: '',
+    };
+  }
 
-        <div className='contact container' id="centre_mobile">
-          <div className="row" >
-            <div className="col s11 l6 z-depth-2" id="contact">
-              <div className="col s11 m4 l12 center-align" id="heading">
-                <h4 className='text-darken-3' id="bottom_margin">Connect with us</h4>
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { name, phone, email, message } = this.state;
+    axios.post('http://127.0.0.1:8000/api/contactform/', { name, phone, email, message });
+  }
+
+  render() {
+    const { name, phone, email, message } = this.state;
+    return (
+      <div id="top_margin">
+        <section id="box_curve_top">
+
+          <div className='contact container' id="centre_mobile">
+            <div className="row" >
+              <div className="col s11 l6 z-depth-2" id="contact">
+                <div className="col s11 m4 l12 center-align" id="heading">
+                  <h4 className='text-darken-3' id="bottom_margin">Connect with us</h4>
+                </div>
+                <form onSubmit={this.onSubmit}>
+                  <div className="col s11 m4 l6" id="column1">
+                    <div className="input-field">
+                      <input type="text" name='name' value={name} onChange={this.onChange} />
+                      <label htmlFor="name">Your Name</label>
+                    </div>
+                    <div className="input-field">
+                      <input type="email" name='email' value={email} onChange={this.onChange} />
+                      <label htmlFor="email">Your email</label>
+                    </div>
+                  </div>
+                  <div className="col s11 m4 l6" id='column2'>
+                    <div className="input-field">
+                      <input type="text" name='phone' value={phone} onChange={this.onChange} />
+                      <label htmlFor="phone">Your Phone Number</label>
+                    </div>
+                    <div className="input-field">
+                      <textarea placeholder='Your message' name="message" value={message} onChange={this.onChange} cols="30" rows="10" className="materialize-textarea"></textarea>
+                    </div>
+                  </div>
+                  <div className="col s11 m4 l12 center-align" id="submit">
+                    <input type="submit" className="btn" value="submit" style={{ 'background-color': '#C33427' }} />
+                  </div>
+                </form>
               </div>
-              <form>
-                <div className="col s11 m4 l6" id="column1">
-                  <div className="input-field">
-                    <input type="text" name='name' />
-                    <label htmlFor="name">Your Name</label>
-                  </div>
-                  <div className="input-field">
-                    <input type="email" name='email' />
-                    <label htmlFor="email">Your email</label>
-                  </div>
-                </div>
-                <div className="col s11 m4 l6" id='column2'>
-                  <div className="input-field">
-                    <input type="text" name='number' />
-                    <label htmlFor="number">Your Phone Number</label>
-                  </div>
-                  <div className="input-field">
-                    <textarea placeholder='Your message' name="message" cols="30" rows="10" className="materialize-textarea"></textarea>
-                  </div>
-                </div>
-                <div className="col s11 m4 l12 center-align" id="submit">
-                  <input type="submit" className="btn" value="submit" style={{ 'background-color': '#C33427' }} />
-                </div>
-              </form>
-            </div>
-            <div className="col s12 m4 l6  margin_top_mobile  center-align" id="follow-us">
-              <h4 id="bottom_margin" className="center-alingnment">Follow Us</h4>
-              <div className="col s12 m10">
+              <div className="col s12 m4 l6  margin_top_mobile  center-align" id="follow-us">
+                <h4 id="bottom_margin" className="center-alingnment">Follow Us</h4>
+                <div className="col s12 m10">
 
-                <div className="row" >
-                  <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-facebook fa-2x" ></i></a>
-                  <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-twitter fa-2x"></i></a>
-                  <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-linkedin fa-2x"></i></a>
-                  <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-google-plus fa-2x"></i></a>
-                  <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-pinterest fa-2x"></i></a>
-                </div>
+                  <div className="row" >
+                    <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-facebook fa-2x" ></i></a>
+                    <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-twitter fa-2x"></i></a>
+                    <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-linkedin fa-2x"></i></a>
+                    <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-google-plus fa-2x"></i></a>
+                    <a href="" style={{ 'color': '#C33427' }} id="icon_space"><i className="fab circle fa-pinterest fa-2x"></i></a>
+                  </div>
 
-                <div className="row margin_top_mobile ">
-                  <h4>Contact</h4>
-                  <p>reachiedcmec@gmail.com</p>
-                  <p>+91 82812 84062</p>
+                  <div className="row margin_top_mobile ">
+                    <h4>Contact</h4>
+                    <p>reachiedcmec@gmail.com</p>
+                    <p>+91 82812 84062</p>
+                  </div>
                 </div>
               </div>
+
             </div>
+          </div>
+
+          <div className="extra_bottom">
 
           </div>
-        </div>
 
-        <div className="extra_bottom">
-
-        </div>
-
-      </section>
-    </div>
-  )
+        </section>
+      </div>
+    )
+  }
 }
 
 export default Contact
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "../index.css";
 import MainSvg from '../Assets/head.png';
 import IEDC from '../Assets/logo.svg';
@@ -7,6 +7,7 @@ import { useEffect } from 'react'
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { Link } from "react-scroll";
+import axios from 'axios';
 
 const Navbar = () => {
 
@@ -18,72 +19,88 @@ const Navbar = () => {
 	return (
 		<div class="navbar-fixed">
 
-		<div className='nav'>
-			<nav className="nav-wrapper accent-1" style={{'background-color':'white'}}>
-				<a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons" style={{'color':'#C33427'}}>menu</i></a>
-				<div className="container">
-					<ul className="right hide-on-small-and-down">
-						<li><Link className='grey-text text-darken-3' activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={400}>Home</Link></li>
-						<li><Link className='grey-text text-darken-3' activeClass="active" to="About" spy={true} smooth={true} offset={-70} duration={400}>About</Link></li>
-						<li><Link className='grey-text text-darken-3' activeClass="active" to="achievements" spy={true} smooth={true} offset={-70} duration={400}>Achievements</Link></li>
-						<li><Link className='grey-text text-darken-3' activeClass="active" to="gallery" spy={true} smooth={true} offset={-70} duration={400}>Gallery</Link></li>
-						<li><Link className='grey-text text-darken-3' activeClass="active" to="team" spy={true} smooth={true} offset={-70} duration={400}>Our Team</Link></li>
-						<li><Link className='grey-text text-darken-3' activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={400}>Contact</Link></li>
-					</ul>
-				</div>
-			</nav>
+			<div className='nav'>
+				<nav className="nav-wrapper accent-1" style={{ 'background-color': 'white' }}>
+					<a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons" style={{ 'color': '#C33427' }}>menu</i></a>
+					<div className="container">
+						<ul className="right hide-on-small-and-down">
+							<li><Link className='grey-text text-darken-3' activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={400}>Home</Link></li>
+							<li><Link className='grey-text text-darken-3' activeClass="active" to="About" spy={true} smooth={true} offset={-70} duration={400}>About</Link></li>
+							<li><Link className='grey-text text-darken-3' activeClass="active" to="achievements" spy={true} smooth={true} offset={-70} duration={400}>Achievements</Link></li>
+							<li><Link className='grey-text text-darken-3' activeClass="active" to="gallery" spy={true} smooth={true} offset={-70} duration={400}>Gallery</Link></li>
+							<li><Link className='grey-text text-darken-3' activeClass="active" to="team" spy={true} smooth={true} offset={-70} duration={400}>Our Team</Link></li>
+							<li><Link className='grey-text text-darken-3' activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={400}>Contact</Link></li>
+						</ul>
+					</div>
+				</nav>
 
-			<ul id="slide-out" className="sidenav" style={{'background-color':'black','padding':'10vh', 'text-align':'center', 'width':'100%'}}>
-				<li><Link className='grey-text' activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={400}>Home</Link></li>
-				<li><Link className='grey-text' activeClass="active" to="About" spy={true} smooth={true} offset={-70} duration={400}>About</Link></li>
-				<li><Link className='grey-text' activeClass="active" to="achievements" spy={true} smooth={true} offset={-70} duration={400}>Achievements</Link></li>
-				<li><Link className='grey-text' activeClass="active" to="gallery" spy={true} smooth={true} offset={-70} duration={400}>Gallery</Link></li>
-				<li><Link className='grey-text' activeClass="active" to="team" spy={true} smooth={true} offset={-70} duration={400}>Our Team</Link></li>
-				<li><Link className='grey-text' activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={400}>Contact</Link></li>
-			</ul>
-		</div>
+				<ul id="slide-out" className="sidenav" style={{ 'background-color': 'black', 'padding': '10vh', 'text-align': 'center', 'width': '100%' }}>
+					<li><Link className='grey-text' activeClass="active" to="home" spy={true} smooth={true} offset={-70} duration={400}>Home</Link></li>
+					<li><Link className='grey-text' activeClass="active" to="About" spy={true} smooth={true} offset={-70} duration={400}>About</Link></li>
+					<li><Link className='grey-text' activeClass="active" to="achievements" spy={true} smooth={true} offset={-70} duration={400}>Achievements</Link></li>
+					<li><Link className='grey-text' activeClass="active" to="gallery" spy={true} smooth={true} offset={-70} duration={400}>Gallery</Link></li>
+					<li><Link className='grey-text' activeClass="active" to="team" spy={true} smooth={true} offset={-70} duration={400}>Our Team</Link></li>
+					<li><Link className='grey-text' activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={400}>Contact</Link></li>
+				</ul>
+			</div>
 		</div>
 	)
 }
 
-const Home = () => {
-	return (
+class Home extends Component {
 
-		
-		<div id=" bottom_margin">
-
-
-		<section className='home main_img' id="box_curve_bottom" style={{'background-color':'white', 'background-image': `url(${MainSvg})`}}>
-		<Navbar/>
-
-<div className="container" id="home" style={{'color':'rgb(223, 218, 218)' }}>
-				<div className="row">
-					<div className="col s12 m6">
-						<div className="iedc_img" style={{'background-image': `url(${IEDC})`, 'height':'15vh'}}></div>
-					</div>
+	constructor() {
+		super();
+		this.state = {
+			announcement: []
+		}
+	}
 
 
+	componentDidMount() {
+		axios.get("http://127.0.0.1:8000/api/announcements/")
+			.then(res => this.setState({ announcement: res.data }))
+	}
 
+	render() {
 
-					<div className="col s12 m5 offset-m1">
-						<h4 id="center_align" className="announcement_header" style={{'color':'#C33427'}}>Announcements</h4>
-						<div className="announcement-box">
-						<div className="col s6 m3" id="announcements_card">
-						<div className="card" id="curve_border">
-							
-							<div className="card-content" style={{'color':'black'}}>
-								<p>> Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/><br/>> Eligendi eius veritatis aut quidem corporis rem ut pariatur harum quas nulla, nihil officiis voluptates tempora, libero aliquid dolorum rerum exercitationem? Repellendus? </p>
+		const display_announcement = this.state.announcement.map(announcements => {
+			return (
+				<div>
+					<p>> {announcements.title}</p>
+					<p>{announcements.description}</p>
+				</div>
+			)
+		})
+		return (
+			<div id=" bottom_margin">
+				<section className='home main_img' id="box_curve_bottom" style={{ 'background-color': 'white', 'background-image': `url(${MainSvg})` }}>
+					<Navbar />
+
+					<div className="container" id="home" style={{ 'color': 'rgb(223, 218, 218)' }}>
+						<div className="row">
+							<div className="col s12 m6">
+								<div className="iedc_img" style={{ 'background-image': `url(${IEDC})`, 'height': '15vh' }}></div>
 							</div>
-							
-						</div>z
-					</div>
+
+							<div className="col s12 m5 offset-m1">
+								<h4 id="center_align" className="announcement_header" style={{ 'color': '#C33427' }}>Announcements</h4>
+								<div className="announcement-box">
+									<div className="col s6 m3" id="announcements_card">
+										<div className="card" id="curve_border">
+											<div className="card-content" style={{ 'color': 'black' }}>
+												<p>{display_announcement}</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
+				</section>
 			</div>
-		</section>
-		</div>
-	)
+		);
+	}
 }
 
-export default Home
+export default Home;
