5ea2ed11d371ff84aa47ee08819451c0cc4d8d1d
nikita <nikitamenon2510@gmail.com>
2020-05-02 10:50:26 +0530

    Images fetched for achievements and gallery + Popups

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "../index.css";
+import axios from 'axios';
+
+import Popup from "./GalleryPopup";
+import '../css/popup_style.css';  
 
 const Gallery = () => {
     useEffect(() => {
@@ -8,72 +12,54 @@ const Gallery = () => {
         M.Materialbox.init(elems);
     });
 
+    const [announcement, setAnnouncement] = useState([]);
+    const [showPopup, setShowPopup] = useState(false);
+
+    useEffect(() => {
+      axios.get("http://127.0.0.1:8000/api/imageupload/")
+      .then(res => setAnnouncement(res.data))
+    });
+
+    
+
+    const display_announcement = announcement.slice(0,5).map(announcements => {
+      return (
+        <div>
+           <div className="col s12 m4" id="bottom_margin">
+           <div className="card-image">
+              <img
+                style={{ "background-size": "cover", margin: "auto", 'height':'300px', 'width':'300px' }}
+                src={announcements.image}
+                alt=""
+                className="responsive-img mobile_space"
+                id="curve_border"
+              />
+              </div>
+            </div>
+        </div>
+      )
+    })
+
     return (
       <div className="gallery section" id="gallery">
+
         <div className="container center">
           <h3 className="text-darken-3 center-align" id="bottom_margin">
             Gallery
           </h3>
           <div className="row">
-            <div className="col s12 m4" id="bottom_margin">
-              <img
-                style={{ "background-size": "cover", height: "50vh", margin: "auto" }}
-                src={require("../Assets/gallery1.png")}
-                alt=""
-                className="materialboxed responsive-img mobile_space"
-                id="curve_border"
-              />
-            </div>
-            <div className="col s12 m4" id="bottom_margin">
-              <img
-                style={{ "background-size": "cover", height: "50vh", margin: "auto" }}
-                src={require("../Assets/ache1.png")}
-                alt=""
-                className="materialboxed responsive-img mobile_space"
-                id="curve_border"
-              />
-            </div>
-            <div className="col s12 m4">
-              <img
-                style={{ "background-size": "cover", height: "50vh", margin: "auto" }}
-                src={require("../Assets/ache2.png")}
-                alt=""
-                className="materialboxed responsive-img mobile_space"
-                id="curve_border"
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col s12 m4" id="bottom_margin">
-              <img
-                style={{ "background-size": "cover", height: "50vh", margin: "auto" }}
-                src={require("../Assets/ache3.png")}
-                alt=""
-                className="materialboxed responsive-img hidepic"
-                id="curve_border"
-              />
-            </div>
-
-            <div className="col s12 m4" id="bottom_margin">
-              <img
-                style={{ "background-size": "cover", height: "50vh", margin: "auto" }}
-                src={require("../Assets/gallery1.png")}
-                alt=""
-                className="materialboxed responsive-img mobile_space hidepic"
-                id="curve_border"
-              />
-            </div>
-            <div className="col s12 m4" id="curve_border">
-              <div
+            {display_announcement}
+         
+            <div className="col s4 m3 valign-wrapper" id="curve_border" onClick={() => setShowPopup(!showPopup)}>
+              <div 
                 style={{
-                                height: "50vh",
-                                width: "80%",
+                               
                                 "text-align": "center",
                                 "background-color": "#C33427",
                                 color: "white",
-                                "margin-top": "0",
+                                margin:'auto'
                             }}
-                className="card-panel waves-effect"
+                className="card-panel gallery_height" 
                 id="curve_border"
               >
                 <i
@@ -85,7 +71,19 @@ const Gallery = () => {
                 <p style={{ "margin-right": "auto", "margin-top": "55%" }}>More</p>
               </div>
             </div>
-          </div>
+
+
+            {showPopup ?  
+<Popup  
+          text='Click "Close Button" to hide popup'  
+          closePopup={() => setShowPopup(!showPopup)}  
+/>  
+: null  
+}  
+
+
+           
+        </div>
         </div>
       </div>
     );
