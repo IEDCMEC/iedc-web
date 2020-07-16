5db27ea5039ea6eb8e8b0e2bc3d24d3cdf9cd938
Vivek R <123vivekr@gmail.com>
2020-07-16 12:36:08 +0530

    update UI

--- frontend/src/Pages/Landing.jsx
+++ frontend/src/Pages/Landing.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Home from "../components/Home";
 import About from "../components/About";
-import Achievements from "../components/Achievements";
-import Gallery from "../components/Gallery";
 import Contact from "../components/Contact";
+import Intro from "../components/intro";
 import "../css/fixed.css";
 
 const Landing = () => {
@@ -11,8 +10,7 @@ const Landing = () => {
       <div className="landing">
         <Home />
         <About />
-        <Achievements />
-        <Gallery />
+        <Intro/>
         <Contact />
       </div>
     );
