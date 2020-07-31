635e6252ce412e8f066f1b51deb80f2b8c2638f0
Vivek R <123vivekr@gmail.com>
2020-07-31 20:31:10 +0530

    version 1

--- frontend/src/Pages/Team/MembersList.js
+++ frontend/src/Pages/Team/MembersList.js
@@ -13,77 +13,77 @@ export const executiveMembers = [{
     },
     {
         id: 3,
-        name: "Ansif",
-        designation: "Tech Team Head",
+        name: "Ansif T A",
+        designation: "Chief Operating Officer",
         imageUrl: require("../../Assets/team/ansif.jpg"),
     },
     {
         id: 4,
-        name: "Ashmi",
-        designation: "Tech Team Head",
+        name: "Ashmi Fathima",
+        designation: "Chief Publicity Officer",
         imageUrl: require("../../Assets/team/ashmi.jpg"),
     },
     {
         id: 5,
-        name: "Medha",
-        designation: "Tech Team Head",
+        name: "Medha Lakshman Rao",
+        designation: "Chief Social Media Officer",
         imageUrl: require("../../Assets/team/medha.jpg"),
     },
     {
         id: 6,
-        name: "Suvarna",
-        designation: "Tech Team Head",
+        name: "Suvarna Sivadas",
+        designation: "Chief Content Officer",
         imageUrl: require("../../Assets/team/suvarna.jpg"),
     },
     {
         id: 7,
-        name: "Vivek",
+        name: "Vivek R",
         designation: "Chief Technology Officer",
         imageUrl: require("../../Assets/team/vivek.jpg"),
+    },
+    {
+        id: 8,
+        name: "Aswin Shanil",
+        designation: "Chief Financial Officer",
+        imageUrl: require("../../Assets/team/aswin.jpg"),
+    },
+    {
+        id: 9,
+        name: "Harinarayanan Shaji",
+        designation: "Chief Design Officer",
+        imageUrl: require("../../Assets/team/hari.jpg"),
     }
 ];
 
 export const firstYears = [{
         id: 1,
         name: "Aditya Anil Kumar",
-        designation: "Tech Team Head",
+        designation: "Webmaster",
         imageUrl: require("../../Assets/team/aditya.jpg"),
     },
     {
         id: 2,
-        name: "Jagath Jijo",
-        designation: "Tech Team Head",
-        imageUrl: require("../../Assets/team/jagath.jpg"),
+        name: "Nikita Menon",
+        designation: "Webmaster",
+        imageUrl: require("../../Assets/team/nikita.jpg"),
     },
     {
         id: 3,
-        name: "Razeen",
-        designation: "Tech Team Head",
-        imageUrl: require("../../Assets/team/razeen.jpg"),
-    },
-    {
-        id: 4,
-        name: "Nikita Menon",
-        designation: "Tech Team Head",
-        imageUrl: require("../../Assets/team/nikita.jpeg"),
-    },
-    {
-        id: 5,
-        name: "Denin Paul",
-        designation: "Tech Team Head",
-        imageUrl: require("../../Assets/team/denin.png"),
-    },
-    {
-        id: 6,
         name: "Aldrin Jenson",
-        designation: "Tech Team Head",
+        designation: "Webmaster",
         imageUrl: require("../../Assets/team/aldrin.jpg"),
     },
     {
-        id: 7,
+        id: 4,
         name: "Arti Bhatia",
-        designation: "Tech Team Head",
+        designation: "Member",
         imageUrl: require("../../Assets/team/arti.jpg"),
+    },
+    {
+        id: 5,
+        name: "Jagath Jijo",
+        designation: "Member",
+        imageUrl: require("../../Assets/team/jagath.jpg"),
     },
 ];
 
@@ -91,13 +91,13 @@ export const firstYears = [{
 export const secondYears = [{
         id: 1,
         name: "Meenakshi",
-        designation: "Tech Team Head",
+        designation: "Member",
         imageUrl: require("../../Assets/team/meenakshi.jpg"),
     },
     {
         id: 2,
         name: "Ameen",
-        designation: "Tech Team Head",
+        designation: "Member",
         imageUrl: require("../../Assets/team/ameen.jpg"),
     },
 ]
