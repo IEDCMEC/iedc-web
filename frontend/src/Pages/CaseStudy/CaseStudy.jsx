a67096ca9f54a6b3095ba9d599c60c99b3a9cdbd
Vivek R <123vivekr@gmail.com>
2020-08-10 14:24:45 +0530

    add case study

--- frontend/src/Pages/CaseStudy/CaseStudy.jsx
+++ frontend/src/Pages/CaseStudy/CaseStudy.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { Component } from "react";
 import './CaseStudy.styles.css'
 import Card from './Card'
-const CaseStudy = () => {
-   return (
-     <div>
-        <div className="row" >
-  
-         
-         <Card image= {require("../../Assets/team/vivek.jpg")}
-            title= "Case Study of Vivek"/>
-         
-       
-         <Card image= {require("../../Assets/team/vivek.jpg")}
-            title= "Case Study of Vivek sdsdsdsdsdsdsd"/>
-         
-      </div>
-      </div>
-   )
+
+class CaseStudy extends Component {
+   constructor() {
+      super();
+      this.state = {
+         case_studies: []
+      }
+   }
+
+   componentDidMount() {
+      fetch('https://raw.githubusercontent.com/IEDCMEC/data/master/case_study.json')
+         .then(response => response.json())
+         .then(data => this.setState({ case_studies: data }));
+   }
+
+   render() {
+      return (
+         <div>
+            <div className="row" >
+               {
+                  this.state.case_studies.map(case_study => {
+                     return(
+                        <Card image={"https://raw.githubusercontent.com/IEDCMEC/data/master/" + case_study.url}
+                           title={case_study.name}
+                           link={case_study.link}
+                           />
+                     )
+                  })
+               }
+            </div>
+         </div>
+      )
+   }
 }
 
-export default CaseStudy
+export default CaseStudy;
