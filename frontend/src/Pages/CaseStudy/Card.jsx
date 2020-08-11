bdaf88ad3aac39c37422124c21ba49bf01e236e9
Vivek R <123vivekr@gmail.com>
2020-08-11 11:41:16 +0530

    css fix

--- frontend/src/Pages/CaseStudy/Card.jsx
+++ frontend/src/Pages/CaseStudy/Card.jsx
@@ -7,11 +7,11 @@ const Card = ({image,title,link}) => {
 <div class="col s12 m6 l3 ">
    <a href={link} target="_blank">
       <div className="container" style={{'padding-top':'70px'}}id="mobcard"> 
-        <div className="card ">
+               <div className="card case_study_card">
            <div className="card-image">
               <img src={image}  style={{ "height": '15em' }} />      
            </div>
-           <div className="card-content center" >
+                  <div className="card-content case_study_content center" >
               <p className="title">{title}</p>
            </div>
         </div>
