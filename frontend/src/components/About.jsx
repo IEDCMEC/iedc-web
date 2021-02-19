73e3b09b5e6667309e31447f60572cc6563aabb4
Vishruth S <vs.vishruth.s@gmail.com>
2021-02-19 13:21:57 +0530

    Bug fixes 1

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,14 +10,14 @@ const About = () => {
         <p className='white-text' >Founded in January in the year 2009 as Entrepreneurship Cell, MEC and revamped as IEDC MEC under the guidance of Kerala Startup Mission as a Govt. of Kerala initiative in 2015, the Innovation and Entrepreneurship Development Cell of Model Engineering college has constantly worked towards helping the community find an intersection between technology and entrepreneurship. We strongly believe that entrepreneurship is the key to unlock the nation’s true potential and therefore strive to promote the idea of job creation rather than job seeking. By consistently hosting a large number of inspiring and innovative events and through our flagship annual national level symposium- ‘Technopreneur’, IEDC MEC, from its humble beginnings is now one of the best functioning IEDCs in the state.</p>
 
         <p className='white-text' >IEDC MEC currently acts as a pre-incubator which helps in providing mentorship and support to the projects of our students and help to scale them into potential startup realities. We are funded by the Kerala Startup Mission (KSUM).</p>
-        <a className="btn waves-effect " style={{'color':'#BF2200', 'background-color':'white','font-weight':'500'}}>
-							<i className="material-icons right">arrow_forward</i>
+        <a className="btn waves-effect " style={{ 'color': '#BF2200', 'background-color': 'white', 'font-weight': '500' }}>
+          <i className="material-icons right">arrow_forward</i>
 							Read More
 			</a>
-            <br/>
-            <br/>
-            <a className="btn waves-effect " style={{'color':'#BF2200', 'background-color':'white','font-weight':'500'}}>
-							<i className="material-icons right">arrow_forward</i>
+        <br />
+        <br />
+        <a href="https://discord.gg/Je6rGaT" className="btn waves-effect " style={{ 'color': '#BF2200', 'background-color': 'white', 'font-weight': '500' }}>
+          <i className="material-icons right">arrow_forward</i>
 							Join The Community
 			</a>
 
--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,41 +1,41 @@
 import React, { Component } from "react";
 import "../index.css";
 
-class Contact extends Component{
+class Contact extends Component {
 
-  render(){
+  render() {
     return (
-      <div id="top_margin" style={{ "background-color": "#122B42"}}>
+      <div id="top_margin" style={{ "background-color": "#122B42" }}>
         <section className="main_img center">
-            <div className="row" style={{marginBottom: "0px", backgroundColor: "#122B42", color: "white"}} id="centre_mobile">
-              <div
-                className="col s12 m12 l12 margin_top_mobile"
-                id="contact"
-              >
-                <h4 id="bottom_margin">
-                  Follow Us
+          <div className="row" style={{ marginBottom: "0px", backgroundColor: "#122B42", color: "white" }} id="centre_mobile">
+            <div
+              className="col s12 m12 l12 margin_top_mobile"
+              id="contact"
+            >
+              <h4 id="bottom_margin">
+                Follow Us
                 </h4>
-                <div className="col s12 m12 l12" id="icons">
-                  <div>
-                    <a href="https://twitter.com/iedc_mec" target="_blank" style={{ color: "white" }} id="icon_space">
-                      <i className="fab circle fa-twitter fa-2x" />
-                    </a>
-                    <a href="http://linkedin.com/company/iedcmec" target="_blank" style={{ color: "white" }} id="icon_space">
-                      <i className="fab circle fa-linkedin fa-2x" />
-                    </a>
-                    <a href="https://www.instagram.com/iedcmec/" target="_blank" style={{ color: "white" }} id="icon_space">
-                      <i className="fab circle fa-instagram fa-2x" />
-                    </a>
-                  <a href="https://www.facebook.com/IEDCMEC/" target="_blank" style={{ color: "white" }} id="icon_space">
-                      <i className="fab circle fa-facebook fa-2x" />
-                    </a>
-                  </div>
-                  </div>
-                  <div style={{paddingTop:"3rem", paddingBottom:"3rem", paddingLeft:"5px"}}>
-                    <h4>Contact</h4>
-                    <h6>iedc@mec.ac.in</h6>
-                  </div>
+              <div className="col s12 m12 l12" id="icons">
+                <div>
+                  <a href="https://twitter.com/iedc_mec" target="_blank" style={{ color: "white" }} id="icon_space">
+                    <i className="fab circle fa-twitter fa-2x" />
+                  </a>
+                  <a href="http://linkedin.com/company/iedcmec" target="_blank" style={{ color: "white" }} id="icon_space">
+                    <i className="fab circle fa-linkedin fa-2x" />
+                  </a>
+                  <a href="https://www.instagram.com/iedcmec/" target="_blank" style={{ color: "white" }} id="icon_space">
+                    <i className="fab circle fa-instagram fa-2x" />
+                  </a>
+                  <a href="https://discord.gg/Je6rGaT" target="_blank" style={{ color: "white" }} id="icon_space">
+                    <i className="fab circle fa-discord fa-2x" />
+                  </a>
+                </div>
               </div>
+              <div style={{ paddingTop: "3rem", paddingBottom: "3rem", paddingLeft: "5px" }}>
+                <h4>Contact</h4>
+                <h6>iedc@mec.ac.in</h6>
+              </div>
+            </div>
           </div>
         </section>
       </div>
