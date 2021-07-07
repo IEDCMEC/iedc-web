e4d63950ff5f2242bc21dedfb80a3ef2f39738a9
Vivek R <123vivekr@gmail.com>
2021-07-07 18:42:41 +0530

    add 2021 magazine

--- src/Pages/Catalyst/Catalyst.jsx
+++ src/Pages/Catalyst/Catalyst.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Catalyst.styles.css";
-
+import Catalyst2021 from "../../Assets/catalyst2021.jpg";
 class Catalyst extends Component {
     constructor() {
         super();
@@ -12,20 +12,28 @@ class Catalyst extends Component {
     render() {
         return (
             <div className="catalyst-card">
-                <div className="catalyst-image"></div>
+                    <img src={Catalyst2021 } className="catalyst-image"/>
 
                 <div className="catalyst-content">
                     <div className="catalyst-headings">
                         <div className="catalyst-heading1">CATALYST 2021</div>
                         <div className="catalyst-heading2">
-                            <span style={{ color: "white" }}>Chief editor</span>
+                            <span style={{ color: "grey" }}>Chief editor</span>
                             <span style={{ fontWeight: "500" }}> Suvarna Sivadas</span>
                         </div>
                     </div>
 
                     <div className="catalyst-buttons">
-                        <div className="catalyst-button">READ ONLINE</div>
-                        <div className="catalyst-button">DOWNLOAD</div>
+                        <div className="catalyst-button">
+                            <a href="https://issuu.com/iedcmec/docs/catalyst_2021_min_" target="_blank" rel="noopener noreferrer">
+                                READ ONLINE
+                            </a>
+                        </div>
+                        <div className="catalyst-button">
+                            <a href="https://drive.google.com/file/d/1B0W1ni6bTOPspthYkTZsTovY6M7doK1y/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                                DOWNLOAD
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
