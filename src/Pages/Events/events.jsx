4b074885cfbfe00c142b10379c0f0656d1405ef6
Amjad Ali <cowboyamjad@gmail.com>
2022-11-23 10:42:47 +0530

    all

---
+++ src/Pages/Events/events.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Eventcard from '../../components/Eventcard';
+import Filter from '../../components/Filter';
+import "./events.css"
+
+const events = () => {
+
+  const events = [
+    {
+      "Name" : "C++ Bootcamp",
+      "Date" : "19-20-2022",
+      "Status" : "Finished",
+      "Domain" : "Online"
+    },
+    {
+      "Name" : "Python Bootcamp",
+      "Date" : "19-20-2022",
+      "Status" : "Finished",
+      "Domain" : "Online"
+    },
+    {
+      "Name" : "Webdev Bootcamp",
+      "Date" : "19-20-2022",
+      "Status" : "Finished",
+      "Domain" : "Online"
+    },
+    {
+      "Name" : "Hardware Bootcamp",
+      "Date" : "19-20-2022",
+      "Status" : "Finished",
+      "Domain" : "Online"
+    },
+
+  ];
+
+  
+  const [filtered, setfiltered] = useState([]) ;
+
+  return (
+    <div className="events">
+      <Filter/>
+      <div className="card-container">
+     {
+      events.map((events) => {
+        return <Eventcard Name = {events.Name} Date = {events.Date} Status = "Finished" Domain = "Online"/>
+      })
+     }
+    </div>
+    </div>
+  )
+}
+
+export default events
---
+++ src/components/Filter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div className="filter-buttons">
+        <button>All</button>
+        <button>Past</button>
+        <button>Upcoming</button>
+    </div>
+  )
+}
+
+export default Filter
