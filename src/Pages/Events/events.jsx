fa5c1e2cecb67038b2af947c7ca9718fac3b8bcd
Amjad Ali <cowboyamjad@gmail.com>
2022-12-06 19:46:20 +0530

    ui changes

--- src/Pages/Events/events.jsx
+++ src/Pages/Events/events.jsx
@@ -1,63 +1,75 @@
 import React from 'react'
 import Eventcard from '../../components/Eventcard';
-import Filter from '../../components/Filter';
 import "./events.css"
+import { useState, useEffect } from 'react';
 
-const events = () => {
+const Events = () => {
+
+  const [selected, setselected] = useState(0);
+  const [filtered, setfiltered] = useState([]);
+  useEffect(() => {
+
+    if (selected == 0)
+      setfiltered(events);
+    else if (selected == 1)
+      setfiltered(events.filter((event) => event.Status === "Finished"));
+    else
+      setfiltered(events.filter((event) => event.Status === "Upcoming"));
+
+
+  }, [selected]);
+
+  console.log(selected);
+  console.log(filtered);
 
   const events = [
     {
-      "Name" : "C++ Bootcamp",
-      "Date" : "19-20-2022",
-      "Status" : "Finished",
-      "Domain" : "Online"
+      "Name": "C++ Bootcamp",
+      "Date": "19-20-2022",
+      "Status": "Finished",
+      "Domain": "Online"
     },
     {
-      "Name" : "Python Bootcamp",
-      "Date" : "19-20-2022",
-      "Status" : "Finished",
-      "Domain" : "Online"
+      "Name": "Python Bootcamp",
+      "Date": "19-20-2022",
+      "Status": "Upcoming",
+      "Domain": "Online"
     },
     {
-      "Name" : "Webdev Bootcamp",
-      "Date" : "19-20-2022",
-      "Status" : "Finished",
-      "Domain" : "Online"
+      "Name": "Webdev Bootcamp",
+      "Date": "19-20-2022",
+      "Status": "Finished",
+      "Domain": "Online"
     },
     {
-      "Name" : "Hardware Bootcamp",
-      "Date" : "19-20-2022",
-      "Status" : "Finished",
-      "Domain" : "Online"
+      "Name": "Hardware Bootcamp",
+      "Date": "19-20-2022",
+      "Status": "Finished",
+      "Domain": "Online"
     },
 
   ];
 
-  
-  // const [filtered, setfiltered] = useState([]) ;
-  //  const [button, setButton] = useState(0) ;
-  //  useEffect(() => {
-  //    first
-   
-  //    return () => {
-  //      second
-  //    }
-  //  }, [button])
-  // console.log(button);
-   
-
   return (
-    <div className="events">
-      <Filter/>
-      <div className="card-container">
-     {
-      events.map((events) => {
-        return <Eventcard Name = {events.Name} Date = {events.Date} Status = "Finished" Domain = "Online"/>
-      })
-     }
+    <div className='event-container'>
+      <div className="filter-buttons">
+        <button onClick={() => { setselected(0) }} className={selected === 0 ? "active" : ""}>All</button>
+        <button onClick={() => { setselected(1) }} className={selected === 1 ? "active" : ""}>Past</button>
+        <button onClick={() => { setselected(2) }} className={selected === 2 ? "active" : ""}>Upcoming</button>
+      </div>
+      <div className="events">
+        <div className="card-container">
+          {
+            filtered.map((events) => {
+              return <Eventcard Name={events.Name} Date={events.Date} Status={events.Status} Domain={events.Domain} />
+            })
+          }
+        </div>
+      </div>
     </div>
-    </div>
+
+
   )
 }
 
-export default events
+export default Events
