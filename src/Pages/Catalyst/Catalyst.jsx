51ce3e6071c760fcb5182b7515e56f5296e5a010
Abhinav P S <146312343+abiinavv@users.noreply.github.com>
2024-09-19 14:35:35 +0530

    Update Catalyst.jsx

--- src/Pages/Catalyst/Catalyst.jsx
+++ src/Pages/Catalyst/Catalyst.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import "./Catalyst.styles.css";
-import Catalyst2021 from "../../Assets/catalyst2021.jpg";
+import Catalyst2022 from "../../Assets/Catalyst_2022_page-0001.jpg";
 
 const Catalyst = () => {
     return (
         <div className="catalyst-card">
-            <img src={Catalyst2021} className="catalyst-image" />
+            <img src={Catalyst2022} className="catalyst-image" />
 
             <div className="catalyst-content">
                 <div className="catalyst-headings">
-                    <div className="catalyst-heading1">CATALYST 2021</div>
+                    <div className="catalyst-heading1">CATALYST 2022</div>
                     <div className="catalyst-heading2">
                         <span style={{ color: "grey" }}>Chief editor</span>
-                        <span style={{ fontWeight: "500" }}> Suvarna Sivadas</span>
+                        <span style={{ fontWeight: "500" }}> Aswathy S Menon</span>
                     </div>
                 </div>
 
                 <div className="catalyst-buttons">
                     <a
-                        href="https://issuu.com/iedcmec/docs/catalyst_2021_min_"
+                        href="https://issuu.com/iedcmec/docs/first_merged_compressed"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         <div className="catalyst-button">READ ONLINE</div>
                     </a>
                     <a
-                        href="https://drive.google.com/file/d/1B0W1ni6bTOPspthYkTZsTovY6M7doK1y/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1R3Sf5V8WMoyGsNNpehYIfnaLwrcq-kd0/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
